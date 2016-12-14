cd431c8e53e9be23f8361a351805b7f43ab476a8
nstrucky <nickstruckmeyer@gmail.com>
2016-12-13 22:26:23 -0500

    Android Nanodegree Submission - 12/12/2016

--- app/src/main/java/com/netjob/basicmusicplayer/MusicLibraryActivity.java
+++ app/src/main/java/com/netjob/basicmusicplayer/MusicLibraryActivity.java
@@ -1,7 +1,15 @@
 package com.netjob.basicmusicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MusicLibraryActivity extends AppCompatActivity {
 
@@ -9,5 +17,28 @@ public class MusicLibraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_library);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ListView tracksListView = (ListView) findViewById(R.id.listview_tracks);
+
+        String[] dummySongs = {"Cool Song", "Metal Stuff", "Back Porch Song", "Classical Gas"};
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                                                    R.layout.textview_tracks_list_item,
+                                                    R.id.textView_tracks_list_item,
+                                                    dummySongs);
+
+        tracksListView.setAdapter(arrayAdapter);
+        tracksListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), NowPlayingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+
 }
--- app/src/main/java/com/netjob/basicmusicplayer/PodcastLibraryActivity.java
+++ app/src/main/java/com/netjob/basicmusicplayer/PodcastLibraryActivity.java
@@ -1,7 +1,12 @@
 package com.netjob.basicmusicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PodcastLibraryActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class PodcastLibraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_podcast_library);
+
+        ListView podcastsListView = (ListView) findViewById(R.id.listview_podcasts);
+
+        String[] dummyEpisodes = {"RadioLab - ep. 1",
+                                  "Freakonomics - ep. 23",
+                                  "NPR Politics ep. 275",
+                                  "Nerp de Derp ep. 4"};
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                R.layout.textview_tracks_list_item,
+                R.id.textView_tracks_list_item,
+                dummyEpisodes);
+
+        podcastsListView.setAdapter(arrayAdapter);
+        podcastsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), NowPlayingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
